970bb55ec39f5eeb8f84f2e4b53a1130bb3398a4
JToaddler <JToaddler@users.noreply.github.com>
2025-06-27 23:37:03 -0400

    removed

--- servlet-sse/src/main/java/com/madlabs/servlet/sse/jaxrs/JaxrsActivator.java
+++
@@ -1,9 +0,0 @@
-package com.madlabs.servlet.sse.jaxrs;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class JaxrsActivator extends Application {
-
-}
